670f3d99a3efc36a081522117e862e1f854e16f5
congle16 <lechicong.reddevil.1610@gmail.com>
2022-04-22 17:22:13 +0700

    update project

--- src/routers/router.js
+++ src/routers/router.js
@@ -4,17 +4,10 @@ const express = require('express');
 const rootRouter = express.Router();
 
 rootRouter.all('/', (req, res, next) => {
+    console.log(req.query);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'X-Requested-With');
     next();
-});
-
-rootRouter.get('/temp', (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
-    res.send({
-        message: "Top Page"
-    })
 });
 
 module.exports = rootRouter;
